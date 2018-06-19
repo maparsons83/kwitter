d4c9860e51b523ed0e9a20abf3e22d12b595f4c6
Matt Parsons <maparsons83@yahoo.com>
2018-06-19 12:05:43 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+            <section className="kwitterapp">
+                <header className="header">
+                    <h1>Welcome to Kwitter!</h1>
+                </header>
+                <section className="register">
+                    <h2>New to Kwitter?  Register here!</h2>
+                        <form>
+                            <label>
+                                <input type="text" placeholder="Email Address" />
+                            </label>
+                            <label>
+                                <input type="text" placeholder="Password" />
+                            </label>
+                            <label>
+                                <input type="text" placeholder="Password" />
+                            </label>
+                            <input type="submit" value="Submit" />
+                        </form>
+                </section>
+                <section className="login">
+                    <h2>Already a member?  Login</h2>
+                        <form>
+                            <label>
+                                <input type="text" placeholder="Email Address" />
+                            </label>
+                            <label>
+                                <input type="text" placeholder="Password" />
+                            </label>
+                            <input type="submit" value="Submit" />
+                        </form>
+                </section>
+            </section>
+    );
+  }
+}
+
+export default App;
